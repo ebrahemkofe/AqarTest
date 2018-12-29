a41dffc6aeb42e293f1488b635b86696ca972892
magdy0000 <magdy.mahmoud0000@outlook.com>
2018-12-29 03:51:12 +0200

    d

--- app/src/main/java/com/example/maggy/aqartest/Home.java
+++ app/src/main/java/com/example/maggy/aqartest/Home.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,6 +45,8 @@ public class Home extends AppCompatActivity {
                     e.putBoolean("intent",false);
                     e.commit();
 
+
+
                     Intent intent = new Intent(getApplicationContext(),Login.class);
                     startActivity(intent);
                     finish();
@@ -71,4 +76,6 @@ public class Home extends AppCompatActivity {
 
         backPressedTime = System.currentTimeMillis();
     }
+
+
 }
