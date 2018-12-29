69d264e9da8b32845d54d48c705ea4142d52360a
mohamedApplication <37400528+mohamedApplication@users.noreply.github.com>
2018-12-29 03:15:08 +0200

    panorama activity still has error

--- app/src/main/java/com/example/maggy/aqartest/Home.java
+++ app/src/main/java/com/example/maggy/aqartest/Home.java
@@ -1,12 +1,17 @@
 package com.example.maggy.aqartest;
 
 import android.content.Intent;
+
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +21,8 @@ public class Home extends AppCompatActivity {
     private TextView mTextMessage;
     private FrameLayout frameLayout;
     private long backPressedTime ;
+
+    Button inside;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -29,7 +36,8 @@ public class Home extends AppCompatActivity {
                     return true;
                 case R.id.navigation_dashboard:
                     Toast.makeText(Home.this,"second",Toast.LENGTH_LONG).show();
-                    return true;
+                    startActivity(new Intent(Home.this, PanoramaView.class));
+                    break;
                 case R.id.navigation_notifications:
                     Toast.makeText(Home.this,"third",Toast.LENGTH_LONG).show();
                     return true;
@@ -59,6 +67,19 @@ public class Home extends AppCompatActivity {
         frameLayout=findViewById(R.id.frame);
         BottomNavigationView navigation = findViewById(R.id.navigationBar);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        inside = findViewById(R.id.inside);
+
+        inside.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = new BlankFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.frame, f).commit();
+            }
+        });
 
     }
     @Override
