8bf4f81e421c4d8711591d68f78a4fabcee11123
mohamedApplication <37400528+mohamedApplication@users.noreply.github.com>
2018-12-29 03:39:58 +0200

    panorama activity still has error

---
+++ app/src/main/java/com/example/maggy/aqartest/PanoramaView.java
@@ -0,0 +1,43 @@
+package com.example.maggy.aqartest;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
+
+public class PanoramaView extends AppCompatActivity {
+
+
+    private GyroscopeObserver gyroscopeObserver;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.panorama_view);
+
+        gyroscopeObserver = new GyroscopeObserver();
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/9);
+
+        PanoramaImageView panoramaImageView =  findViewById(R.id.panorama);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeObserver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeObserver.unregister();
+    }
+}
