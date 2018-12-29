738e4ef72b51c49f2c9ea7a0e60baafc8af8f751
mohamedApplication <37400528+mohamedApplication@users.noreply.github.com>
2018-12-29 04:31:11 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/maggy/aqartest/Home.java

--- app/src/main/java/com/example/maggy/aqartest/Home.java
+++ app/src/main/java/com/example/maggy/aqartest/Home.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +22,9 @@ public class Home extends AppCompatActivity {
     private FrameLayout frameLayout;
     private long backPressedTime ;
 
-    //fdfksldsvm
+    Button inside;
+
+
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -61,6 +68,23 @@ public class Home extends AppCompatActivity {
         frameLayout=findViewById(R.id.frame);
         BottomNavigationView navigation = findViewById(R.id.navigationBar);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+       inside = findViewById(R.id.inside);
+
+       inside.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Fragment f =new BlankFragment();
+
+               FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+               ft.replace(R.id.frame,f,"fra");
+
+               ft.commit();
+
+           }
+       });
+
 
     }
     @Override
